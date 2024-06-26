66f039c11bc1d5f7f68b284f5fb9f8fa4826c743
dmitkov28 <mitkov.dimitar@yahoo.com>
2024-06-26 19:50:29 +0300

    fix layout height

--- src/routes/_protected/speech-to-text/index.tsx
+++ src/routes/_protected/speech-to-text/index.tsx
@@ -3,7 +3,7 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_protected/speech-to-text/")({
   component: () => (
-    <div className="w-full flex h-screen md:p-12 p-3">
+    <div className="w-full flex flex-grow md:p-12 p-3">
       <div className="flex flex-grow md:justify-between justify-center items-center mb-4 flex-wrap gap-y-8 md:gap-y-0">
         <Upload />
       </div>
